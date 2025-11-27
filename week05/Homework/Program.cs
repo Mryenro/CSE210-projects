501fd45e157036e8bb32f642ffe067515e5828ab
Mryenro <hbouhou@byupathway.edu>
2025-11-27 18:21:08 +1100

    homework assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Hamza Bouhou", "programming");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+
+
+        MathAssignment mathAssignment1 = new MathAssignment("Hamza Bouhou", "programming", "Section 7.2", "Problem 10-20");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Hamid Cherkaoui", "AI research", "The dangers of AI");
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
+
+
     }
 }
