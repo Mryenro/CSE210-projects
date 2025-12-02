060022ce9f6e8ce2fb0924226f54f0278e510d10
Mryenro <hbouhou@byupathway.edu>
2025-12-02 22:42:58 +1100

    learning activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square = new Square("red", 12);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+
+        Rectangle rectangle = new Rectangle("blue", 10, 15);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+
+        Circle circle = new Circle("yellow", 7);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"this is the color: {shape.GetColor()}, and this is the area: {shape.GetArea()}");
+        }
+
+
+
+
     }
 }
